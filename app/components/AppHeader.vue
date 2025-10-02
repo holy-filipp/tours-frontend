a93bfc559885d9120c2602308b32e5e136b0db7f
Filipp <im@filipp.su>
2025-10-02 18:24:21 +0400

    initial

---
+++ app/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <UHeader>
+    <template #title>
+      <Logo />
+    </template>
+  </UHeader>
+</template>
+
+<script lang="ts" setup>
+import type { NavigationMenuItem } from '@nuxt/ui'
+import Logo from "~/assets/images/logo.svg"
+
+const route = useRoute()
+
+const items = computed<NavigationMenuItem[]>(() => [
+  {
+    label: 'Docs',
+    to: '/docs/getting-started',
+    active: route.path.startsWith('/docs/getting-started')
+  },
+  {
+    label: 'Components',
+    to: '/docs/components',
+    active: route.path.startsWith('/docs/components')
+  },
+  {
+    label: 'Figma',
+    to: 'https://go.nuxt.com/figma-ui',
+    target: '_blank'
+  },
+  {
+    label: 'Releases',
+    to: 'https://github.com/nuxt/ui/releases',
+    target: '_blank'
+  }
+])
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <AppHeader />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
