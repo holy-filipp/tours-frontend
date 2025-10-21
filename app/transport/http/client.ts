061d87a675462a305b64d20097736724a01cda81
Filipp <im@filipp.su>
2025-10-21 09:43:47 +0400

    sync

--- app/transport/http/client.ts
+++ app/transport/http/client.ts
@@ -9,6 +9,33 @@ export class ApiError extends Error {
   }
 }
 
-export const client = {
+type IInitData = RequestInit & {
+  json?: string
+}
 
+export const client = {
+  request: async <T>(path: string, initData: IInitData = {}) => {
+    const headers = new Headers(initData?.headers)
+    headers.set('Accept', 'application/json')
+
+    initData.headers = headers
+
+    if (initData?.json) {
+      initData.body = JSON.stringify(initData?.json)
+    }
+
+    const response = await fetch(path, initData)
+
+    try {
+      const data = await response.json()
+
+      if (response.status > 299) {
+
+      }
+
+      return data as Promise<T>
+    } catch {
+      throw Error('Error parsing JSON')
+    }
+  }
 }
